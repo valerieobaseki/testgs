b1d9289e248ab0dce3c468e2ebb81884b8190212
valerieobaseki <34221811+valerieobaseki@users.noreply.github.com>
2020-12-07 17:21:36 -0600

    Update my.js

--- my.js
+++ my.js
@@ -1,10 +1,10 @@
 function testGS(){
-    var url = "https://script.googleusercontent.com/macros/echo?user_content_key=aeAYZeI6HkX-ljx1FpMZEst8onQkH8uG6LsMjD1JvxvDMb9yZyt3DKgraStv66FL2kfZzJsp6WoeBL9xLOgnv6XforothQmem5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnMrfKI7o3W-zki4XTlk2h1_sxrxB4Dm-gUBuJZ8Wh8uj9w3eanhbrf7Oy_om9_nY0ndO6ACmiFd3GGaU2pP6Ntk&lib=MLavx5prdqfjzSqxLRTTOKdUj6pQy2WoH";
+    var url = "https://script.googleusercontent.com/macros/echo?user_content_key=yt-Qtglrya7GGwnUwotvnCsznsm274NJyH5Lk1OST2ykSw4CJ3dUKiTQZU2gD6bjn8XtUBbOq3-sxba-mjLQUASvy22lZ5Fom5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnA3puq2BYbE7rV7oWIl31Xq-i5AhQYBwWyEbddyrwKPJ0on5HHmRh9toLyd0geonWDHYlvxnKr4OGGaU2pP6Ntk&lib=MBNzBteGq5n9YUmnt-nME9dUj6pQy2WoH";
 
     fetch(url)
     .then(d => d.json())
     .then(d => {
-    document.getElementById("app").textContent = d[0].message;
+    document.getElementById("app").textContent = d[0].message[1].first;
     });
 
 
