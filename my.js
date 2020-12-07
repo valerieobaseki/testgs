1b5332b6195c81b3e75018c0337dbdafafff44c1
valerieobaseki <34221811+valerieobaseki@users.noreply.github.com>
2020-12-07 17:13:20 -0600

    Update my.js

--- my.js
+++ my.js
@@ -4,7 +4,7 @@ function testGS(){
     fetch(url)
     .then(d => d.json())
     .then(d => {
-    document.getElementById("app").textContent = d[0].status;
+    document.getElementById("app").textContent = d[0].message;
     });
 
 
