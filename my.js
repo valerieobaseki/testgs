d53bd0a71072bd69f690e64a7dfca6cac5f45344
valerieobaseki <34221811+valerieobaseki@users.noreply.github.com>
2020-12-07 17:15:39 -0600

    Update my.js

--- my.js
+++ my.js
@@ -1,5 +1,5 @@
 function testGS(){
-    var url = "https://script.googleusercontent.com/macros/echo?user_content_key=HTucSM56ru3FLH6FO2slFfwOx_jWQVRKrizs9ApEw06B0w6uLavh2mN0ag5AohFC4O_TcECgX0Cs9acolJdcQpAfhycX9UTEm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnHSUmmmmHVOnPyMmqtWMppYfnby_mwwXUGvQNDlmxplavudFWVGcbNw0VBegouUmz5cdv3aNHr6K&lib=MLavx5prdqfjzSqxLRTTOKdUj6pQy2WoH";
+    var url = "https://script.googleusercontent.com/macros/echo?user_content_key=aeAYZeI6HkX-ljx1FpMZEst8onQkH8uG6LsMjD1JvxvDMb9yZyt3DKgraStv66FL2kfZzJsp6WoeBL9xLOgnv6XforothQmem5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnMrfKI7o3W-zki4XTlk2h1_sxrxB4Dm-gUBuJZ8Wh8uj9w3eanhbrf7Oy_om9_nY0ndO6ACmiFd3GGaU2pP6Ntk&lib=MLavx5prdqfjzSqxLRTTOKdUj6pQy2WoH";
 
     fetch(url)
     .then(d => d.json())
